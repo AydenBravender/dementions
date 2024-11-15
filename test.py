229c5251a960d7bb2e73ed76c06a01f553be3a3f
AydenBravender <aydenbravender@gmail.com>
2024-11-14 19:17:21 -0700

    added speech to text

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 from ToneAnalyzer import ToneAnalyzer
 
-sentence = "your gay as hell"
+
+input_file_path = "Andrew.m4a"  # Replace with your .m4a or .wav file
+converted_audio_path = ToneAnalyzer.convert_to_mono(input_file_path)
+sentence = ToneAnalyzer.transcribe_audio(converted_audio_path)
+print("Transcription:", sentence)
 tone_analyzer = ToneAnalyzer(sentence)
 sentiment_scores = tone_analyzer.analyze_tone()
-print(sentiment_scores)
+stage = sentiment_scores['neg']
+print(stage)
+stage = (stage*3)+1
+
+print(stage)
+
