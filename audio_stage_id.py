a36b896898958d1a73358f6aa6086cae6ce265ca
Ayden Bravender <144071841+AydenBravender@users.noreply.github.com>
2024-11-16 09:34:53 -0700

    Create audio_stage_id.py

---
+++ audio_stage_id.py
@@ -0,0 +1,14 @@
+from ToneAnalyzer import ToneAnalyzer
+
+
+input_file_path = "/home/ayden/Desktop/aydenprj/nathacks/models/Test.m4a"  # Replace with your .m4a or .wav file
+converted_audio_path = ToneAnalyzer.convert_to_mono(input_file_path)
+sentence = ToneAnalyzer.transcribe_audio(converted_audio_path)
+print("Transcription:", sentence)
+tone_analyzer = ToneAnalyzer(sentence)
+sentiment_scores = tone_analyzer.analyze_tone()
+stage = sentiment_scores['neg']
+print(stage)
+stage = (stage*3)+1
+
+print(stage)
