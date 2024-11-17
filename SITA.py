71d013eb1338dcdb6dd0e19652dfb8950291aea7
plasticx153 <plasticx153@gmail.com>
2024-11-16 22:44:10 -0700

    fixed bug w @staticmethod
    I have no idea what that does but it works now

--- SITA.py
+++ SITA.py
@@ -8,7 +8,6 @@ from vosk import Model, KaldiRecognizer
 from pydub import AudioSegment
 import os
 import google.generativeai as genai
-from fluency import ToneAnalyzer
 import spacy
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -31,12 +30,11 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 class Sita:
     def __init__(self, filepath):
         self.audio = filepath
-
-        converted_audio = self.convert_to_mono(filepath)
+        converted_audio = self.convert_to_mono(self.audio)
         self.sentence = self.whisper_transcribe(converted_audio) #CAN REPLACE WITH VOSK TRANSCRIBE
         self.gemini_score = self.gemini_prompt(self.sentence)
         self.sentiment_scores = self.analyze_tone(self.sentence)
-        self.wps = self.find_wps(self.sentence) #not a score, score_calc function takes care of that, just sloppy coding on my end, and too lazy - Lucas
+        self.wps = self.find_wps(converted_audio, self.sentence) #not a score, score_calc function takes care of that, just sloppy coding on my end, and too lazy - Lucas
         self.score_pair = self.score_calc(self.wps, self.gemini_score, self.sentiment_scores)
         self.outlier_sentences = self.tokenization(self.sentence)
 
@@ -49,20 +47,20 @@ class Sita:
         score = sentiment_scores['neg']*100
         return score
     
-    def gemini_prompt(text):
+    def gemini_prompt(self, text):
         prompt = "\n Does the text above suggest that the speaker has dementia, on a scale of 0-100? Please respond with only an integer, 0 being no dementia at all, and 100 being severe dementia"
         response = model.generate_content(text + prompt)
         output = int(response.text.split()[0])
         return output
     
-    def whisper_transcribe(file_path):
+    def whisper_transcribe(self, file_path):
         #Load whisper model
         model = whisper.load_model("base")
         #transribe using wav file
         result = model.transcribe(file_path)
         return result["text"]
 
-    def tokenization(text):
+    def tokenization(self, text):
         #load model
         nlp = spacy.load("en_core_web_sm")
         #load sentencizer
@@ -78,7 +76,7 @@ class Sita:
 
         #embeddings
         model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-        sentences = [sentence for sentence in doc.sents] #doc.sents is an interator, we are creating a list of sentence objects at once so we do not have to reiterate doc.sents
+        sentences = [str(sentence) for sentence in doc.sents] #doc.sents is an interator, we are creating a list of sentence objects at once so we do not have to reiterate doc.sents
         embeddings = np.array(model.encode(sentences))
 
         #average embeddings
@@ -94,7 +92,7 @@ class Sita:
         #return an array of irrelevant sentecnes
         return irrelevant_sentences
 
-    def vosk_transcribe(file_path, model_path=vosk_model_path):
+    def vosk_transcribe(self, file_path, model_path=vosk_model_path):
         # Load the Vosk model
         model = Model(model_path)
         
@@ -126,7 +124,7 @@ class Sita:
         return transcription
 
     #input .wav file and transcript
-    def find_wps(converted_file, text):
+    def find_wps(self, converted_file, text):
         with contextlib.closing(wave.open(converted_file,'r')) as f:
             frames = f.getnframes()
             rate = f.getframerate()
@@ -136,6 +134,7 @@ class Sita:
 
         return wps
 
+    @staticmethod
     def convert_to_mono(input_file, output_file="converted_mono_audio.wav", target_rate=16000):
         if not isinstance(input_file, str):
             raise ValueError(f"Expected a file path (str), got {type(input_file)}")
@@ -151,10 +150,12 @@ class Sita:
         return output_file
     
     #tone should be result of class.analyze_tone()
-    def score_calc(wps, gemini, tone):
+    def score_calc(self, wps, gemini, tone):
         wps = (wps/1.9) * 100
         if wps > 100:
             wps = 100
         speech_cohesiveness = (gemini * 0.7) + (wps * 0.3)
         return speech_cohesiveness, tone
     
+toneanal = Sita("ResponseAudios/MergedAudio.wav")
+print(toneanal.score_pair)
